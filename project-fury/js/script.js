fadd6ed46ebf60a93ac71b8e094b338246b597af
danghung168 <atulamutsu168@gmai.com>
2016-12-09 16:12:18 +0700

    fix link and fix scroll

--- project-fury/js/script.js
+++ project-fury/js/script.js
@@ -1,16 +1,29 @@
 $('.bxslider').bxSlider({
-    // auto: true,
+    auto: true,
     autoControls: true,
-    // autoDelay: 5000
+    autoDelay: 5000
 });
 $(document).ready(function () {
     $(window).scroll(function () {
-        var scrollBot = $(document).height() - 2 * $(window).height() - $(window).scrollTop();
-        var top = $(".right-home").height();
+        var scrollBot = $(document).height() -  $(window).height() - $(window).scrollTop() - 536;
         if (scrollBot > 0){
-            $(".left-home").removeClass("position-relative").addClass("position-fixed");
+            $(".left-home").css({
+                'position':'fixed',
+                'top':0,
+                'bottom':'inherit'
+            });
+            // $(".left-home").animate({
+            //     position: 'fixed',
+            //    top: 0
+            // });
         }else{
-            $(".left-home").addClass("position-relative").removeClass("position.fixed");
+            // $(".left-home").css("position",'absolute').css('bottom',0).css('top','inherit');
+
+            $(".left-home").css({
+                position: 'absolute',
+                top: 'inherit',
+                bottom: 0
+            });
         }
     });
 
