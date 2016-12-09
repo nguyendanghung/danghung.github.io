7bfd9bc4f3fdfc80fda40740e4b170e06d7eb9df
danghung168 <atulamutsu168@gmai.com>
2016-12-09 10:41:18 +0700

    pull

--- project/js/script.js
+++ project/js/script.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
     var height1 = $(document).height() - 2 * $(window).height() - $(window).scrollTop();
     var top1 = $(window).scrollTop();
     // alert(top1);
-    if (top1 === 2509){
+    if (height1 < 0){
         alert('rwerwe');
         $('.left-home').removeClass('position-fixed').addClass('position-relative').css("top", top1+"px");
     }else {
@@ -17,9 +17,9 @@ $(document).ready(function () {
     // ----button search---
     $(".top-search button").click(function () {
         if($(this).hasClass("active") === true ){
-            $(this).removeClass("active");
+            $(this).removeClass("active").siblings().addClass("active");
         }else {
-            $(this).addClass("active").next().removeClass("active");
+            $(this).addClass("active").siblings().removeClass("active");
         }
 
     })
